ad5b2cffbca90c73c0ec6bdef7b187598c7b5cf0
Anas Kadamany <anaskadamany@hotmail.com>
2025-04-08 19:52:06 +0300

    adding the admin sign-in Functionality

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
-let express = require("express");
-let app = express();
+const express = require("express");
+const dotenv = require("dotenv");
+const authRoutes = require("./routers/authRoutes");
 
-app.listen("8080", () => {
-  console.log("the server work on port 8080");
+dotenv.config();
+const app = express();
+
+app.use(express.json()); // to put the json in the req.body
+app.use("/api/auth", authRoutes);
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log("Server running on port 8080");
 });
