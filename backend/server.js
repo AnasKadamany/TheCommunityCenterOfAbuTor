aad6b8d875445930d72ef543cc105189476675f8
Anas Kadamany <anaskadamany@hotmail.com>
2025-04-10 16:15:21 +0300

    added the news API Functionalities

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,15 @@ const express = require("express");
 const dotenv = require("dotenv");
 const authRoutes = require("./routers/authRoutes");
 const eventRoutes = require("./routers/eventRoutes");
+const newsRoutes = require("./routers/newsRoutes");
+
 dotenv.config();
 const app = express();
 
 app.use(express.json()); // to put the json in the req.body
 app.use("/api/auth", authRoutes);
 app.use("/api/events", eventRoutes);
+app.use("/api/news", newsRoutes);
 
 app.listen(process.env.PORT || 8080, () => {
   console.log("Server running on port 8080");
